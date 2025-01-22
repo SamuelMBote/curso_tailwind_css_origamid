cfdb68913951bfc0183fd40e20aeea6f9e699f73
IntegraseOnline <36938470+SamuelMBote@users.noreply.github.com>
2025-01-21 22:23:13 -0300

    Projeto até o ponto da section Eventos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ['./src/**/*.{html,js,ts}', './*.html'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '.5rem',
+        sm: '1rem',
+      },
+    },
+    extend: {
+      colors: {
+        verde: {
+          200: '#ACEF75',
+          300: '#91EE77',
+          400: '#17E880',
+          800: '#2E482C',
+          800: '#16281F',
+          900: '#0F1C15',
+          950: '#030504',
+        },
+      },
+    },
   },
   plugins: [],
 };
